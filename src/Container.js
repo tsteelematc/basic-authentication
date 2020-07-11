1c0f5ffe96682ab3c17d4d40e3ff8049d1df9f2c
Tom Steele <tsteele@madisoncollege.edu>
2020-07-11 10:46:17 -0500

    Container retyped...
    And thought about how to teach it...
    
    Not sure if documenting the how is proper, or if my instincts would just take over ? ? ?
    
    Or maybe this helps other instructors ? ? ?
    
    Maybe something here, that wasn't there before : - )

---
+++ src/Container.js
@@ -0,0 +1,64 @@
+// import something from somewhere : - )
+import React from 'react';
+
+//
+// This is a function ! ! !
+//
+// Need to be able to squint at it and see ! ! !
+//
+//const Container = () => {}
+
+//
+// A function that takes an object and that object
+// is expected to have a property named children. 
+// 
+// This is destructuring ! ! !
+//
+// Show some Babel desctruturing REPL code here ! ! !
+//
+// const o = {
+// 	children: [
+//     	"Taylor"
+//       , "Jack"
+//     ]
+// }
+
+// // Destructuring ! ! !
+// const { children } = o;
+// window.alert(children);
+
+
+// // Array swap with destructuring...
+// let a = 1;
+// let b = 2;
+
+// [b, a]  = [a, b];
+
+// console.log(a);
+// console.log(b);
+
+// First with just div with containers ! ! !
+// const Container = ({ children }) => (
+//     <div>
+//         { containers }
+//     </div>
+// )
+
+// Then add styles...
+const Container = ({ children }) => (
+    <div
+        style={styles.container}
+    >
+        { containers }
+    </div>
+)
+
+const styles = {
+    container: {
+        margin: '0 auto'
+        , padding: '50px 100px'
+    }
+}
+
+// Export the function as the default export...
+export default Container;
